c555e3043a628ffcf169937d9a7dfa36cb3bea47
Kalikant <kalikant@users.noreply.github.com>
2017-11-05 20:21:18 +0800

    Create AsynchronousProducer.java

---
+++ AsynchronousProducer.java
@@ -0,0 +1,42 @@
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.*;
+import org.apache.kafka.clients.producer.*;
+
+public class AsynchronousProducer {
+
+   public static void main(String[] args) throws Exception{
+      String topicName = "AsynchronousProducerTopic";
+          String key = "Key1";
+          String value = "Value-1";
+
+          
+          Properties props = new Properties();
+          try {
+			   props.load(new FileInputStream("kafka.properties"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+          
+      Producer<String, String> producer = new KafkaProducer <>(props);
+
+      ProducerRecord<String, String> record = new ProducerRecord<>(topicName,key,value);
+
+      producer.send(record, new MyProducerCallback());
+      System.out.println("AsynchronousProducer call completed");
+      producer.close();
+
+   }
+
+}
+
+    class MyProducerCallback implements Callback{
+
+       @Override
+       public  void onCompletion(RecordMetadata recordMetadata, Exception e) {
+        if (e != null)
+            System.out.println("AsynchronousProducer failed with an exception");
+                else
+                        System.out.println("AsynchronousProducer call Success:");
+       }
+   }
